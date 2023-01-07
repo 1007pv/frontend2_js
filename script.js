7abcbe4f3fa15284b20777aa1136b9e640bd3732
Pankaj Verma <pankajv@adobe.com>
2023-01-07 17:44:10 +0530

    submit 1

--- script.js
+++ script.js
@@ -8,20 +8,39 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  return console.log(arr);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(function(el){
+  return console.log(el);
+  })
 }
 
 function addData() {
   //Write your code here
+  arr.push({id:4, name:"susan", age:"20", profession:"intern"})
+  return console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here
+  //Write your code here  
+  const profession="admin";
+  let newArr = arr.filter(function(item){
+    return item.profession!=profession;
+  })
+  return console.log(newArr);
 }
+
+let arrNew = [
+  { id: 010, designation: "HR", salary: "18000", location: "Noida" },
+  { id: 020, designation: "Marketing", salary: "20000", location: "Delhi" },
+  { id: 030, designation: "Counsellor", salary: "19000", location: "Banglore" },
+];
 
 function concatenateArray() {
   //Write your code here
+let concateArray = arr.concat(arrNew);
+return console.log(concateArray);
 }
